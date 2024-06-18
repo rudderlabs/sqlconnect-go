011a7fd19eae06c0c466756306f6ec58f45d0017
Aris Tzoumas <atzoumas@rudderstack.com>
2024-06-18 11:50:04 +0300

    feat(snowflake): support key-pair authentication (#103)
    # Description
    
    Introducing new configuration fields to support snowflake's [Key-pair
    authentication](https://docs.snowflake.com/en/user-guide/key-pair-auth):
    - `useKeyPairAuth` - enables key-pair authentication
    - `privateKey` - the private key that will be used
    - `privateKeyPassphrase` - (optional) the passphrase to use for
    decrypting the private key if it is encrypted
    
    ## Linear Ticket
    
    resolves PRO-2869
    
    ## Security
    
    - [x] The code changed/added as part of this pull request won't create
    any security issues with how the software is being used.

--- sqlconnect/internal/snowflake/config.go
+++ sqlconnect/internal/snowflake/config.go
@@ -1,10 +1,15 @@
 package snowflake
 
 import (
+	"crypto/rsa"
 	"encoding/json"
+	"encoding/pem"
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/snowflakedb/gosnowflake"
+	"github.com/youmark/pkcs8"
 )
 
 type Config struct {
@@ -12,9 +17,14 @@ type Config struct {
 	Warehouse string `json:"warehouse"`
 	DBName    string `json:"dbname"`
 	User      string `json:"user"`
-	Password  string `json:"password"`
 	Schema    string `json:"schema"`
 	Role      string `json:"role"`
+
+	Password string `json:"password"`
+
+	UseKeyPairAuth       bool   `json:"useKeyPairAuth"`
+	PrivateKey           string `json:"privateKey"`
+	PrivateKeyPassphrase string `json:"privateKeyPassphrase"`
 
 	KeepSessionAlive  bool `json:"keepSessionAlive"`
 	UseLegacyMappings bool `json:"useLegacyMappings"`
@@ -22,13 +32,23 @@ type Config struct {
 
 func (c Config) ConnectionString() (dsn string, err error) {
 	sc := gosnowflake.Config{
-		User:      c.User,
-		Password:  c.Password,
-		Account:   c.Account,
-		Database:  c.DBName,
-		Warehouse: c.Warehouse,
-		Schema:    c.Schema,
-		Role:      c.Role,
+		Authenticator: gosnowflake.AuthTypeSnowflake,
+		User:          c.User,
+		Password:      c.Password,
+		Account:       c.Account,
+		Database:      c.DBName,
+		Warehouse:     c.Warehouse,
+		Schema:        c.Schema,
+		Role:          c.Role,
+	}
+
+	if c.UseKeyPairAuth {
+		sc.Authenticator = gosnowflake.AuthTypeJwt
+		privateKey, err := c.ParsePrivateKey()
+		if err != nil {
+			return "", fmt.Errorf("parsing private key: %w", err)
+		}
+		sc.PrivateKey = privateKey
 	}
 
 	if c.KeepSessionAlive {
@@ -47,4 +67,42 @@ func (c Config) ConnectionString() (dsn string, err error) {
 
 func (c *Config) Parse(configJSON json.RawMessage) error {
 	return json.Unmarshal(configJSON, c)
+}
+
+func (c *Config) ParsePrivateKey() (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(normalisePem(c.PrivateKey)))
+	if block == nil {
+		return nil, errors.New("decoding private key failed")
+	}
+
+	var opts [][]byte
+	if len(c.PrivateKeyPassphrase) > 0 {
+		opts = append(opts, []byte(c.PrivateKeyPassphrase))
+	}
+
+	rsaPrivateKey, err := pkcs8.ParsePKCS8PrivateKeyRSA(block.Bytes, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("parsing private key: %w", err)
+	}
+	return rsaPrivateKey, nil
+}
+
+// normalisePem formats the content of certificates and keys by adding necessary newlines around specific markers.
+func normalisePem(content string) string {
+	// Remove all existing newline characters
+	formattedContent := strings.ReplaceAll(content, "\n", "")
+
+	// Add a newline after specific BEGIN markers
+	formattedContent = strings.Replace(formattedContent, "-----BEGIN CERTIFICATE-----", "-----BEGIN CERTIFICATE-----\n", 1)
+	formattedContent = strings.Replace(formattedContent, "-----BEGIN RSA PRIVATE KEY-----", "-----BEGIN RSA PRIVATE KEY-----\n", 1)
+	formattedContent = strings.Replace(formattedContent, "-----BEGIN ENCRYPTED PRIVATE KEY-----", "-----BEGIN ENCRYPTED PRIVATE KEY-----\n", 1)
+	formattedContent = strings.Replace(formattedContent, "-----BEGIN PRIVATE KEY-----", "-----BEGIN PRIVATE KEY-----\n", 1)
+
+	// Add a newline before and after specific END markers
+	formattedContent = strings.Replace(formattedContent, "-----END CERTIFICATE-----", "\n-----END CERTIFICATE-----\n", 1)
+	formattedContent = strings.Replace(formattedContent, "-----END RSA PRIVATE KEY-----", "\n-----END RSA PRIVATE KEY-----\n", 1)
+	formattedContent = strings.Replace(formattedContent, "-----END ENCRYPTED PRIVATE KEY-----", "\n-----END ENCRYPTED PRIVATE KEY-----\n", 1)
+	formattedContent = strings.Replace(formattedContent, "-----END PRIVATE KEY-----", "\n-----END PRIVATE KEY-----\n", 1)
+
+	return formattedContent
 }
