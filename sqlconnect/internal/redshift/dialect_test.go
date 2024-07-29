359c2304c54acccd7045dabaccf4bfe004cfe5f0
Aris Tzoumas <atzoumas@rudderstack.com>
2024-07-29 12:21:04 +0300

    fix(redshift): dialect#parserelationref doesn't honour enable_case_sensitive_identifier (#146)
    # Description
    
    Using `Dialect#NormaliseIdentifier` from within
    `Dialect#ParseRelationRef`
    
    ## Security
    
    - [x] The code changed/added as part of this pull request won't create
    any security issues with how the software is being used.

--- sqlconnect/internal/redshift/dialect_test.go
+++ sqlconnect/internal/redshift/dialect_test.go
@@ -43,6 +43,25 @@ func TestDialect(t *testing.T) {
 
 		normalised = d.NormaliseIdentifier(`"Sh""EmA".TABLE."ColUmn"`)
 		require.Equal(t, `"sh""ema".table."column"`, normalised, "all parts should be normalised")
+
+		t.Run("case sensitive", func(t *testing.T) {
+			d := dialect{caseSensitive: true}
+
+			normalised := d.NormaliseIdentifier("column")
+			require.Equal(t, "column", normalised, "column name should be normalised to lowercase")
+
+			normalised = d.NormaliseIdentifier("COLUMN")
+			require.Equal(t, "column", normalised, "column name should be normalised to lowercase")
+
+			normalised = d.NormaliseIdentifier(`"ColUmn"`)
+			require.Equal(t, `"ColUmn"`, normalised, "quoted column name should be preserved")
+
+			normalised = d.NormaliseIdentifier(`TaBle."ColUmn"`)
+			require.Equal(t, `table."ColUmn"`, normalised, "unquoted parts should be normalised")
+
+			normalised = d.NormaliseIdentifier(`"Sh""EmA".TABLE."ColUmn"`)
+			require.Equal(t, `"Sh""EmA".table."ColUmn"`, normalised, "unquoted parts should be normalised")
+		})
 	})
 
 	t.Run("parse relation", func(t *testing.T) {
@@ -65,5 +84,29 @@ func TestDialect(t *testing.T) {
 		parsed, err = d.ParseRelationRef(`"CaTa""LoG".ScHeMA."TaBle"`)
 		require.NoError(t, err)
 		require.Equal(t, sqlconnect.RelationRef{Catalog: "cata\"log", Schema: "schema", Name: "table"}, parsed)
+
+		t.Run("case sensitive", func(t *testing.T) {
+			d := dialect{caseSensitive: true}
+
+			parsed, err := d.ParseRelationRef("table")
+			require.NoError(t, err)
+			require.Equal(t, sqlconnect.RelationRef{Name: "table"}, parsed)
+
+			parsed, err = d.ParseRelationRef("TABLE")
+			require.NoError(t, err)
+			require.Equal(t, sqlconnect.RelationRef{Name: "table"}, parsed)
+
+			parsed, err = d.ParseRelationRef(`"TaBle"`)
+			require.NoError(t, err)
+			require.Equal(t, sqlconnect.RelationRef{Name: `TaBle`}, parsed)
+
+			parsed, err = d.ParseRelationRef(`ScHeMA."TaBle"`)
+			require.NoError(t, err)
+			require.Equal(t, sqlconnect.RelationRef{Schema: "schema", Name: "TaBle"}, parsed)
+
+			parsed, err = d.ParseRelationRef(`"CaTa""LoG".ScHeMA."TaBle"`)
+			require.NoError(t, err)
+			require.Equal(t, sqlconnect.RelationRef{Catalog: "CaTa\"LoG", Schema: "schema", Name: "TaBle"}, parsed)
+		})
 	})
 }
