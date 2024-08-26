f9e924191b6e00efcf89f2ff13a92871adff96b7
Aris Tzoumas <atzoumas@rudderstack.com>
2024-08-26 11:03:21 +0300

    chore: cleanup test schemas periodically

---
+++ sqlconnect/cmd/cleanup.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/tidwall/sjson"
+
+	"github.com/rudderlabs/sqlconnect-go/sqlconnect"
+	"github.com/rudderlabs/sqlconnect-go/sqlconnect/internal/bigquery"
+	"github.com/rudderlabs/sqlconnect-go/sqlconnect/internal/databricks"
+	"github.com/rudderlabs/sqlconnect-go/sqlconnect/internal/redshift"
+	"github.com/rudderlabs/sqlconnect-go/sqlconnect/internal/snowflake"
+	"github.com/rudderlabs/sqlconnect-go/sqlconnect/internal/trino"
+)
+
+func main() {
+	cleanupConfigs := []cleanupConfig{
+		{Env: "BIGQUERY_TEST_ENVIRONMENT_CREDENTIALS", Type: bigquery.DatabaseType},
+		{Env: "DATABRICKS_TEST_ENVIRONMENT_CREDENTIALS", Type: databricks.DatabaseType, Fn: func(s string) string {
+			s, _ = sjson.Set(s, "catalog", "hive_metastore")
+			return s
+		}},
+		{Env: "DATABRICKS_TEST_ENVIRONMENT_CREDENTIALS", Type: databricks.DatabaseType, Fn: func(s string) string {
+			s, _ = sjson.Set(s, "catalog", "sqlconnect")
+			return s
+		}},
+		{Env: "REDSHIFT_DATA_TEST_ENVIRONMENT_CREDENTIALS", Type: redshift.DatabaseType},
+		{Env: "REDSHIFT_TEST_ENVIRONMENT_CREDENTIALS", Type: redshift.DatabaseType},
+		{Env: "SNOWFLAKE_TEST_ENVIRONMENT_CREDENTIALS", Type: snowflake.DatabaseType},
+		{Env: "TRINO_TEST_ENVIRONMENT_CREDENTIALS", Type: trino.DatabaseType},
+	}
+
+	for _, c := range cleanupConfigs {
+		configJSON, ok := os.LookupEnv(c.Env)
+		if !ok {
+			log.Fatalf("%s environment variable not set", c.Env)
+		}
+		if c.Fn != nil {
+			configJSON = c.Fn(configJSON)
+		}
+		db, err := sqlconnect.NewDB(c.Type, []byte(configJSON))
+		if err != nil {
+			log.Fatalf("[%s] failed to create db: %v", c.Type, err)
+		}
+		schemas, err := db.ListSchemas(context.Background())
+		if err != nil {
+			log.Fatalf("[%s] failed to list schemas: %v", c.Type, err)
+		}
+		for _, schema := range schemas {
+			if strings.Contains(strings.ToLower(schema.Name), "tsqlcon_") {
+				err := db.DropSchema(context.Background(), schema)
+				if err != nil {
+					log.Printf("[%s] failed to drop schema: %v", c.Type, err)
+				} else {
+					log.Printf("[%s] dropped schema %s", c.Type, schema)
+				}
+			}
+		}
+	}
+}
+
+type cleanupConfig struct {
+	Type string
+	Env  string
+	Fn   func(string) string
+}
